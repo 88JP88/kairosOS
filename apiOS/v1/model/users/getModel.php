55da6c07191cffb4c1283f3e321fedacc1b31649
paezgit88 <120344753+paezgit88@users.noreply.github.com>
2024-03-22 13:49:33 -0500

    A

--- apiOS/v1/model/users/getModel.php
+++ apiOS/v1/model/users/getModel.php
@@ -174,7 +174,7 @@ public static function getSites($dta) {
 if($filter=="filter"){
 
         if($param=="placeId"){
-            $query = mysqli_query($conectar, "SELECT s.siteId, s.clientId, s.infoSite, s.placeId, JSON_UNQUOTE(JSON_EXTRACT(p.infoPlace, '$[0].info.name')) AS name FROM generalSites s JOIN generalPlaces p ON p.placeId = s.placeId WHERE s.clientId = '$clientId' AND s.placeId IN (SELECT placeId FROM generalPlaces WHERE clientId = '$clientId' AND JSON_UNQUOTE(JSON_EXTRACT(infoPlace, '$.info.name')) LIKE '%$value%')");
+            $query = mysqli_query($conectar, "SELECT s.siteId, s.clientId, s.infoSite, s.placeId,JSON_EXTRACT(p.infoPlace, '$[0].info.name') AS name FROM generalSites s JOIN generalPlaces p ON p.placeId = s.placeId WHERE s.clientId = '$clientId' AND s.placeId IN (SELECT placeId FROM generalPlaces WHERE clientId = '$clientId' AND JSON_EXTRACT(infoPlace, '$[0].info.name') LIKE '%$value%')");
 
         }
         else{
