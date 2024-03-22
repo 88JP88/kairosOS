beaf213e778d14fc5f12148fd4d2bba8a2c6d5db
paezgit88 <120344753+paezgit88@users.noreply.github.com>
2024-03-22 14:55:28 -0500

    A

--- apiOS/v1/model/users/getModel.php
+++ apiOS/v1/model/users/getModel.php
@@ -280,172 +280,149 @@ if ($numRows > 0) {
                             
         
 }
-    public static function getOrders($dta) {
+   
+public static function getElements($dta) {
             
                 
 
 
-        // Asegúrate de proporcionar la ruta correcta al archivo de conexión a la base de datos
-        
-            // Realiza la conexión a la base de datos (reemplaza conn() con tu propia lógica de conexión)
-            $conectar = conn();
+    // Asegúrate de proporcionar la ruta correcta al archivo de conexión a la base de datos
     
-            // Verifica si la conexión se realizó correctamente
-            if (!$conectar) {
-                return "Error de conexión a la base de datos";
-            }
-    
-            
-                
+        // Realiza la conexión a la base de datos (reemplaza conn() con tu propia lógica de conexión)
+        $conectar = conn();
 
-            // Escapa los valores para prevenir inyección SQL
-            $clientId = mysqli_real_escape_string($conectar, $dta['clientId']);
-            $filter = mysqli_real_escape_string($conectar, $dta['filter']);
-            $param = mysqli_real_escape_string($conectar, $dta['param']);
-            $value = mysqli_real_escape_string($conectar, $dta['value']);
-           
-    
-            if($filter=="all"){
+        // Verifica si la conexión se realizó correctamente
+        if (!$conectar) {
+            return "Error de conexión a la base de datos";
+        }
 
         
-                $query= mysqli_query($conectar,"SELECT gor.orderId,gor.carId,gor.clientId,gor.userId,gor.shopperId,gor.storeId,gor.totalAmount,gor.subTotalAmount,gor.orderProgress,gor.saver,gor.paymentMethod,gor.returnCash,gor.transactionStatus,gor.numberProducts,gor.numberPacks,gor.inDate,gor.inTime,gor.incId,gor.paymentReference,gor.payWith,gor.bankEntity,gc.customerName,gc.customerLastName,gs.storeName,gor.orderPayload,gor.deliveryMethod,gor.deliveryAdd,gor.deliveryPerson,gdel.deliveryName,gdel.deliveryLastName,gdel.distanceRules,gor.deliveryStatus FROM generalOrders gor JOIN generalCustomers gc ON gor.shopperId=gc.customerId JOIN generalStores gs ON gor.storeId=gs.storeId JOIN generalDelivery gdel ON gdel.deliveryId=gor.deliveryPerson where gor.clientId='$clientId' ORDER BY gor.incId DESC LIMIT 50");
+            
 
-               // $query= mysqli_query($conectar,"SELECT gc.customerId,gc.clientId,gc.customerName,gc.customerLastName,gc.customerMail,gc.customerPoints,gc.customerPhone,gc.customerStars,gc.customerType,gc.isActive,gr.pointsEq,gr.pointsValue FROM generalCustomers gc JOIN generalRules gr ON gr.clientId=gc.clientId where gc.clientId='$clientId'");
-            }
-            
+        // Escapa los valores para prevenir inyección SQL
+        $clientId = mysqli_real_escape_string($conectar, $dta['clientId']);
+        $filter = mysqli_real_escape_string($conectar, $dta['filter']);
+        $param = mysqli_real_escape_string($conectar, $dta['param']);
+        $value = mysqli_real_escape_string($conectar, $dta['value']);
+       
+
+        if($filter=="all"){
+
+    
+    
+            $query = mysqli_query($conectar, "SELECT e.elementId, e.clientId, e.infoElement, e.siteId, JSON_EXTRACT(s.infoSite, '$[0].info.name') AS sname,JSON_EXTRACT(p.infoPlace, '$[0].info.name') AS pname FROM generalElements e JOIN  generalSites s ON e.siteId=s.siteId JOIN generalPlaces p ON p.placeId=s.placeId WHERE e.clientId = '$clientId'");
+        }
         
-    if($filter=="byStore"){
     
-            
-            
-        $query= mysqli_query($conectar,"SELECT gor.orderId,gor.carId,gor.clientId,gor.userId,gor.shopperId,gor.storeId,gor.totalAmount,gor.subTotalAmount,gor.orderProgress,gor.saver,gor.paymentMethod,gor.returnCash,gor.transactionStatus,gor.numberProducts,gor.numberPacks,gor.inDate,gor.inTime,gor.incId,gor.paymentReference,gor.payWith,gor.bankEntity,gc.customerName,gc.customerLastName,gs.storeName,gor.orderPayload,gor.deliveryMethod,gor.deliveryAdd,gor.deliveryPerson,gdel.deliveryName,gdel.deliveryLastName,gdel.distanceRules,gor.deliveryStatus FROM generalOrders gor JOIN generalCustomers gc ON gor.shopperId=gc.customerId JOIN generalStores gs ON gor.storeId=gs.storeId JOIN generalDelivery gdel ON gdel.deliveryId=gor.deliveryPerson where gor.clientId='$clientId' and gor.$param='$value' ORDER BY gor.incId DESC LIMIT 50");
-    
-    
-    }
-            if($query){
-                $numRows = mysqli_num_rows($query);
+if($filter=="filter"){
+
+        if($param=="placeId"){
+            $query = mysqli_query($conectar, "SELECT s.siteId, s.clientId, s.infoSite, s.placeId,JSON_EXTRACT(p.infoPlace, '$[0].info.name') AS name FROM generalSites s JOIN generalPlaces p ON p.placeId = s.placeId WHERE s.clientId = '$clientId' AND s.placeId IN (SELECT placeId FROM generalPlaces WHERE clientId = '$clientId' AND JSON_EXTRACT(infoPlace, '$[0].info.name') LIKE '%$value%')");
+
+        }
+        else{
+    $query = mysqli_query($conectar, "SELECT s.siteId, s.clientId, s.infoSite, s.placeId, JSON_EXTRACT(p.infoPlace, '$[0].info.name') AS name FROM generalSites s JOIN  generalPlaces p ON p.placeId=s.placeId WHERE s.clientId = '$clientId' AND JSON_EXTRACT(s.infoSite, '$[0].info.$param') LIKE '%$value%'");
+        }
+          
+
+}
+        if($query){
+            $numRows = mysqli_num_rows($query);
 
 if ($numRows > 0) {
-                $response="true";
-                $message="Consulta exitosa";
-                $status="202";
-                $apiMessage="¡Ordenes seleccionadas ($numRows)!";
-                $values=[];
+            $response="true";
+            $message="Consulta exitosa";
+            $status="202";
+            $apiMessage="¡Repartidores seleccionados ($numRows)!";
+            $values=[];
 
-                while ($row = $query->fetch_assoc()) {
-                    $value=[
-                        'orderId' => $row['orderId'],
-                        'carId' => $row['carId'],
-                        'clientId' => $row['clientId'],
-                        'vendor' => $row['userId'],
-                        'shopperId' => $row['shopperId'],
-                        'storeId' => $row['storeId'],
-                        'total' => $row['totalAmount'],
-                        
-                        'subTotal' => $row['subTotalAmount'],
-                        'orderProgress' => $row['orderProgress'],
-                        'saver' => $row['saver'],
-                        'paymentMethod' => $row['paymentMethod'],
-                        'exchange' => $row['returnCash'],
-                        'paymentStatus' => $row['transactionStatus'],
-                        'numberProducts' => $row['numberProducts'],
-                        'numberPacks' => $row['numberPacks'],
-                        'inDate' => $row['inDate'],
-                        'inTime' => $row['inTime'],
-                        'orderNumber' => $row['incId'],
-                        'paymentReference' => $row['paymentReference'],
-                        'payWith' => $row['payWith'],
-                        'bankAccount' => $row['bankEntity'],
-                        'customer' => $row['customerName'].' '.$row['customerLastName'],
-                        'storeName' => $row['storeName'],
-                        'orderPayload' => $row['orderPayload'],
-                        'deliveryAdd' => $row['deliveryAdd'],
-                        'deliveryMethod' => $row['deliveryMethod'],
-                        'deliveryPerson' => $row['deliveryPerson'],
-                        'deliveryName' => $row['deliveryName'],
-                        'deliveryLastName' => $row['deliveryLastName'],
-                        'distanceRules' => $row['distanceRules'],
-                        'deliveryStatus' => $row['deliveryStatus']
-                        
-                    ];
-                    
-                    array_push($values,$value);
-                }
-                
-                $row = $query->fetch_assoc();
-               // return json_encode(['products'=>$values]);
-                
-                // Crear un array separado para el objeto 'response'
-                $responseData = [
-                    'response' => [
-                        'response' => $response,
-                        'message' => $message,
-                        'apiMessage' => $apiMessage,
-                        'status' => $status,
-                        'sentData'=>$dta
-                    ],
-                    'orders' => $values
+            while ($row = $query->fetch_assoc()) {
+                $value=[
+                    'elementId' => $row['elementId'],
+                    'clientId' => $row['clientId'],
+                    'siteId' => $row['siteId'],
+                    'siteName' => json_decode($row['sname']),
+                    'placeName' => json_decode($row['pname']),
+                    'infoElement' => json_decode($row['infoElement'], true)[0]
                 ];
-                
-                return json_encode($responseData);
-            }else {
-                // La consulta no arrojó resultados
-                $response="false";
-                $message="Error en la consulta";
-                $status="204";
-                $apiMessage="¡La consulta no produjo resultados, filas seleccionadas ($numRows)!";
-                $values=[];
-                $value = [
-                    
-                ];
-                $responseData = [
-                    'response' => [
-                        'response' => $response,
-                        'message' => $message,
-                        'apiMessage' => $apiMessage,
-                        'status' => $status,
-                        'sentData'=>$dta
-                    ],
-                    'orders' => $values
-                ];
-                array_push($values,$value);
-                
-    
-        //echo json_encode($students) ;
-        return json_encode($responseData);
-            }
-
-            //  return "true";
-            //echo "ups! el id del repo está repetido , intenta nuevamente, gracias.";
-            }else{
-                $response="false";
-                $message="Error en la consulta: " . mysqli_error($conectar);
-                $status="404";
-                $apiMessage="¡Ordenes no selleccionados con éxito!";
-                $values=[];
-
-                $value = [
-                    
-                ];
-                $responseData = [
-                    'response' => [
-                        'response' => $response,
-                        'message' => $message,
-                        'apiMessage' => $apiMessage,
-                        'status' => $status,
-                        'sentData'=>$dta
-                    ],
-                    'orders' => $values
-                ];
-                array_push($values,$value);
-                
-    
-        //echo json_encode($students) ;
-        return json_encode($responseData);
-                                }
-
-                                
             
+                array_push($values, $value);
+            }
+            
+            $row = $query->fetch_assoc();
+           // return json_encode(['products'=>$values]);
+            
+            // Crear un array separado para el objeto 'response'
+            $responseData = [
+                'response' => [
+                    'response' => $response,
+                    'message' => $message,
+                    'apiMessage' => $apiMessage,
+                    'status' => $status,
+                    'sentData'=>$dta
+                ],
+                'elements' => $values
+            ];
+            
+            return json_encode($responseData);
+        }else {
+            // La consulta no arrojó resultados
+            $response="false";
+            $message="Error en la consulta";
+            $status="204";
+            $apiMessage="¡La consulta no produjo resultados, filas seleccionadas ($numRows)!";
+            $values=[];
+            $value = [
+                
+            ];
+            $responseData = [
+                'response' => [
+                    'response' => $response,
+                    'message' => $message,
+                    'apiMessage' => $apiMessage,
+                    'status' => $status,
+                    'sentData'=>$dta
+                ],
+                'elements' => $values
+            ];
+            array_push($values,$value);
+            
+
+    //echo json_encode($students) ;
+    return json_encode($responseData);
+        }
+
+        //  return "true";
+        //echo "ups! el id del repo está repetido , intenta nuevamente, gracias.";
+        }else{
+            $response="false";
+            $message="Error en la consulta: " . mysqli_error($conectar);
+            $status="404";
+            $apiMessage="¡Repartidores no seleccionados con éxito!";
+            $values=[];
+
+            $value = [
+                
+            ];
+            $responseData = [
+                'response' => [
+                    'response' => $response,
+                    'message' => $message,
+                    'apiMessage' => $apiMessage,
+                    'status' => $status,
+                    'sentData'=>$dta
+                ],
+                'elements' => $values
+            ];
+            array_push($values,$value);
+            
+
+    //echo json_encode($students) ;
+    return json_encode($responseData);
+                            }
+
+                            
+        
 }
        
     }
