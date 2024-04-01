b645899caa07081d33b0747d75d532054ab80a64
88JP88 <gitjuan9@gmail.com>
2024-04-01 18:51:44 -0500

    commit

--- apiOS/v1/model/users/getModel.php
+++ apiOS/v1/model/users/getModel.php
@@ -665,7 +665,7 @@ if ($numRows > 0) {
                     'clientId' => $row['clientId'],
                     'parentId' => $row['parentId'],
                     'parentName' => $row['parentName'],
-                    'parentInfo' => json_decode($row['parentInfo'],true)[0],
+                    'parentInfo' => $row['parentInfo'],
                     'infoCategory' => json_decode($row['infoCategory'], true)[0]
                 ];
             
