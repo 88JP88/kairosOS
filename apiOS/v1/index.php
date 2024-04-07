06a7712f3341a0eda54750321f676772e2dfb20c
88JP88 <gitjuan9@gmail.com>
2024-04-07 11:23:32 -0500

    commit

--- apiOS/v1/index.php
+++ apiOS/v1/index.php
@@ -3,7 +3,7 @@
 require 'flight/Flight.php';
 require_once 'database/db_users.php';
 require_once 'model/users/postModel.php';
-require_once 'model/users/getModel.php';
+//require_once 'model/users/getModel.php';
 //require_once 'model/users/responses.php';
 //require 'model/modelSecurity/authModule.php';
 //require_once 'env/domain.php';
