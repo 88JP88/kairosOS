a363bea906d940eecf7082c0504ca3619c2d4254
88JP88 <gitjuan9@gmail.com>
2024-04-07 11:22:31 -0500

    commit

--- apiOS/v1/index.php
+++ apiOS/v1/index.php
@@ -6,7 +6,7 @@ require_once 'model/users/postModel.php';
 require_once 'model/users/getModel.php';
 require_once 'model/users/responses.php';
 require 'model/modelSecurity/authModule.php';
-require_once 'env/domain.php';
+//require_once 'env/domain.php';
 
 //require_once 'kronos/postLog.php';
 
