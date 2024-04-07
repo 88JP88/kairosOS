d8e7c796bbd67d7de3c7e0df6f761d88e36a7893
88JP88 <gitjuan9@gmail.com>
2024-04-07 11:18:32 -0500

    commit

--- apiOS/v1/index.php
+++ apiOS/v1/index.php
@@ -8,7 +8,7 @@ require_once 'model/users/responses.php';
 require 'model/modelSecurity/authModule.php';
 require_once 'env/domain.php';
 require_once 'kronos/postLog.php';
-require_once 'model/users/getModel.php';
+
 
 
 Flight::route('POST /pp/', function () {
