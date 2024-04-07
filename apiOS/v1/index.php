eaa0b25c52c6b0aa8fdfd3a3a2278ca120b6b213
88JP88 <gitjuan9@gmail.com>
2024-04-07 11:21:46 -0500

    commit

--- apiOS/v1/index.php
+++ apiOS/v1/index.php
@@ -1,7 +1,14 @@
 <?php
 
 require 'flight/Flight.php';
+require_once 'database/db_users.php';
+require_once 'model/users/postModel.php';
+require_once 'model/users/getModel.php';
+require_once 'model/users/responses.php';
+require 'model/modelSecurity/authModule.php';
+require_once 'env/domain.php';
 
+require_once 'kronos/postLog.php';
 
 
 Flight::route('GET /pp', function () {
