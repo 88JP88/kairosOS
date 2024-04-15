a19dc4136616fdb531cf37ecca87aac56c986b50
88JP88 <gitjuan9@gmail.com>
2024-04-15 15:17:32 -0500

    commit

--- apiOS/v1/model/users/postModel.php
+++ apiOS/v1/model/users/postModel.php
@@ -680,7 +680,7 @@ class modelPost {
                     $infoOrder = [
                         [
                             "info" => [
-                                "infoProducts" =>json_encode($dta['products'],true),
+                                "infoProducts" =>json_decode($dta['products'],true),
                                 "infoPayload" => $dta['payload']
                             ],
                             "params" => [
