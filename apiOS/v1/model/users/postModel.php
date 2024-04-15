cc29c7fbd88c7f8a7361456e1220ea5e4017a922
88JP88 <gitjuan9@gmail.com>
2024-04-15 15:06:34 -0500

    commit

--- apiOS/v1/model/users/postModel.php
+++ apiOS/v1/model/users/postModel.php
@@ -680,7 +680,7 @@ class modelPost {
                     $infoOrder = [
                         [
                             "info" => [
-                                "infoProducts" =>json_decode( $infoProducts),
+                                "infoProducts" =>json_decode($infoProducts),
                                 "infoPayload" => $infoPayload
                             ],
                             "params" => [
