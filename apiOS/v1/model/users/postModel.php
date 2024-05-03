b083a6b194d0ac710b52927017092b4a281efd6b
88JP88 <gitjuan9@gmail.com>
2024-05-03 18:26:32 -0500

    commit

--- apiOS/v1/model/users/postModel.php
+++ apiOS/v1/model/users/postModel.php
@@ -2214,7 +2214,7 @@ class modelPut{
                                                     $query5 = mysqli_query($conectar, "UPDATE generalOrders 
                                                     SET infoOrder = JSON_SET(infoOrder, '$[0].info.infoOrder.orderStatus.status', 'finished') 
                                                     WHERE clientId = '$clientId' AND orderId = '$orderIds'");
-                                                    $value="finished";
+                                                    $value="finished123";
                                                 }
 
                                                 
