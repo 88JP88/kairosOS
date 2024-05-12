bef300f5af723b031c5e7b695cee427566f1c3c1
paezgit88 <120344753+paezgit88@users.noreply.github.com>
2024-05-12 16:55:32 -0500

    A

--- apiOS/v1/model/users/getModel.php
+++ apiOS/v1/model/users/getModel.php
@@ -178,7 +178,7 @@ if($filter=="filter"){
 
         }
         if($param=="placeIdCarSites"){
-            $query = mysqli_query($conectar, "SELECT s.siteId, s.clientId, s.infoSite, s.placeId,JSON_EXTRACT(p.infoPlace, '$[0].info.name') AS name FROM generalSites s JOIN generalPlaces p ON p.placeId = s.placeId WHERE s.clientId = '$clientId' AND s.placeId = '$value' and JSON_EXTRACT(s.infoSite, '$[0].params.siteType')='place'");
+            $query = mysqli_query($conectar, "SELECT s.siteId, s.clientId, s.infoSite, s.placeId,JSON_EXTRACT(p.infoPlace, '$[0].info.name') AS name FROM generalSites s JOIN generalPlaces p ON p.placeId = s.placeId WHERE s.clientId = '$clientId' AND s.placeId = '$value' and JSON_EXTRACT(s.infoSite, '$[0].params.siteType')='site'");
 
         }
         if($param=="placeIdCarSitesWork"){
